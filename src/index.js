a9bb504f02554830ba739ab93c9cb46520782a58
Martin <mmaina290@gmail.com>
2023-10-10 14:05:02 +0300

    tasks container

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    createTask(e.target.newTaskDescription.value)
+    //task reset
+    task.reset()
+
+  })
 });
+
+function createTask(list) {
+  let ul = document.querySelector('#tasks')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', justDelete)
+  btn.textContent = 'x'
+  ul.textContent = `${list} `
+  ul.appendChild(btn)
+
+
+}
+
+function justDelete(e) {
+  e.target.parentNode.remove()
+}
